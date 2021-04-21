52ded6fc0aaf1011ed2772e61fa836452b1a95fc
Silke Marie <silk2918@stud.kea.dk>
2021-04-21 13:54:29 +0200

    kvittering ændringer

--- MariosPizzaria/src/KasseApparat.java
+++ MariosPizzaria/src/KasseApparat.java
@@ -6,49 +6,51 @@ public class KasseApparat {
   private String navn;
   private int pris;
   private int antal;
+  private int subTotal;
   private int total;
   private final LocalDateTime salgsTidspunkt = LocalDateTime.now();
   private ArrayList<Pizza> pizzaListe = new ArrayList<>();
+
 
   Pizza pizza = new Pizza(1, 50, "Hawaii", "ananas mm.");
 
   public ArrayList<Pizza> getPizzaListe() {
     return pizzaListe;
   }
-  
+
   public void lavPizza(int nummer, int pris, String navn, String ingredienser) {
     Pizza pizza = new Pizza(nummer, pris, navn, ingredienser);
     pizzaListe.add(pizza);
   }
 
-  public int subTotal() {
-    int subTotal = pizza.getPris() * antal;
+  /*public int subTotal() {
+    int subTotal = pizza.getPris();
     return subTotal();
-  }
-  
+  }*/
+
   public int totalPris() {
-    int total = 0;
-    for (int i = 0; i < pizzaListe.size(); i++) {
-      total += subTotal();
+    int subTotal = 0;
+    for (int i = 0; i < pizzaListe.size(); i++) { //vil gerne ind i ArrayListen og hente prisen for hver pizza i det i et loop
+      subTotal += pizzaListe.get(i).getPris(); //pizzaListe.getPris(i)
     }
     return total;
   }
 
-  public void udskrivKvittering() {
+  /*public void udskrivKvittering() {
     System.out.println(salgsTidspunkt);
     for (int i = 0; i < pizzaListe.size(); i++) {
       System.out.println(pizzaListe.get(i));
 
     }
-  }
+  }*/
 
   @Override
   public String toString() {
     return "Kvittering\n" +
-        "Antal pizzaer: " + antal +
-        "\nSalgstidspunkt: " + salgsTidspunkt +
+        "Antal pizzaer: " + pizzaListe.size() +
         "\nPris: " + total +
-        "\n kr.";
+        " kr." +
+        "\nSalgstidspunkt: " + salgsTidspunkt;
 
   }
 }
