6ce86b83229e8c17ae5c1a51dca78d8501565e0d
chris <csamsinges@gmail.com>
2021-04-22 12:50:10 +0200

    tilføjet do-while til ordre metoden

--- MariosPizzaria/src/Ordrer.java
+++ MariosPizzaria/src/Ordrer.java
@@ -23,31 +23,38 @@ public class Ordrer {
     public void ordre() {
 
         int choice;
+        boolean valid;
         choice = input.nextInt();
 
         System.out.println("Ringer kunden ind eller kommer kunden ind fra gaden? Tryk 1 for ringende, 2 for gående");
+        do {
+            valid = true;
 
-        switch (choice) {
-            case 1:
-                ordrerNummer++;
-                ringende++;
-                System.out.println("Hvad er dit navn?");
-                navn = input.nextLine();
-                System.out.println("Hvad skal du have at spise?");
-                ArrayList<Pizza> telefonPizzaer =  kasseApparat.pizzaValg();
-                Ordrer ordrer = new Ordrer("Ringede",navn,ordrerNummer,telefonPizzaer);
-                ordrerListe.add(ordrer);
-                break;
-            case 2:
-                ordrerNummer++;
-                gaaende++;
-                System.out.println("Hvad skal du have at spise?");
-                ArrayList<Pizza> gaaendePizzaer =  kasseApparat.pizzaValg();
-                Ordrer ordrer2 = new Ordrer("Gående",navn,ordrerNummer,gaaendePizzaer);
-                ordrerListe.add(ordrer2);
-                break;
-        }
+            switch (choice) {
+                case 1:
+                    ordrerNummer++;
+                    ringende++;
+                    System.out.println("Hvad er dit navn?");
+                    navn = input.nextLine();
+                    System.out.println("Hvad skal du have at spise?");
+                    ArrayList<Pizza> telefonPizzaer = kasseApparat.pizzaValg();
+                    Ordrer ordrer = new Ordrer("Ringede", navn, ordrerNummer, telefonPizzaer);
+                    ordrerListe.add(ordrer);
+                    break;
+                case 2:
+                    ordrerNummer++;
+                    gaaende++;
+                    System.out.println("Hvad skal du have at spise?");
+                    ArrayList<Pizza> gaaendePizzaer = kasseApparat.pizzaValg();
+                    Ordrer ordrer2 = new Ordrer("Gående", navn, ordrerNummer, gaaendePizzaer);
+                    ordrerListe.add(ordrer2);
+                    break;
+                default:
+                    valid = false;
+                    System.out.println("Dit valg er ikke gyldigt!");
+            }
+        }while (!valid);
     }
-    }
+}
 
 
